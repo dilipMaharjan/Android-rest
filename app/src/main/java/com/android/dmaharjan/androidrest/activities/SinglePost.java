8ca2ff8e7296a859e06353365dd3e59c27c5931c
crazdrms <dilipmlove@gmail.com>
2017-03-04 18:33:02 +0545

    update and delete post

--- app/src/main/java/com/android/dmaharjan/androidrest/activities/SinglePost.java
+++ app/src/main/java/com/android/dmaharjan/androidrest/activities/SinglePost.java
@@ -1,7 +1,13 @@
 package com.android.dmaharjan.androidrest.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.dmaharjan.androidrest.R;
@@ -15,16 +21,19 @@ import retrofit2.Response;
 
 public class SinglePost extends AppCompatActivity {
     private PostService postService;
+    private Post post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_post);
-        Integer id = getIntent().getExtras().getInt("postId");
+        final Integer id = getIntent().getExtras().getInt("postId");
         postService = RestApi.getAPIService();
+        post = new Post();
         postService.getPost(id).enqueue(new Callback<Post>() {
             @Override
             public void onResponse(Call<Post> call, Response<Post> response) {
+                post = response.body();
                 TextView textViewTitle = (TextView) findViewById(R.id.textViewTitle);
                 TextView textViewContent = (TextView) findViewById(R.id.textViewContent);
                 textViewTitle.setText(response.body().getTitle().toUpperCase());
@@ -34,6 +43,65 @@ public class SinglePost extends AppCompatActivity {
             @Override
             public void onFailure(Call<Post> call, Throwable t) {
 
+            }
+        });
+        findViewById(R.id.updatePost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SinglePost.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.new_post, null);
+                dialogBuilder.setView(dialogView);
+                final EditText editTextTitle = (EditText) dialogView.findViewById(R.id.title);
+                final EditText editTextContent = (EditText) dialogView.findViewById(R.id.content);
+                final EditText editTextUserId = (EditText) dialogView.findViewById(R.id.userId);
+                Button buttonSave = (Button) dialogView.findViewById(R.id.buttonSave);
+                buttonSave.setText("Update");
+                editTextTitle.setText(post.getTitle());
+                editTextContent.setText(post.getBody());
+                editTextUserId.setText(post.getUserId().toString());
+                final AlertDialog alertDialog = dialogBuilder.create();
+                alertDialog.show();
+                buttonSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Post post = new Post();
+                        post.setTitle(editTextTitle.getText().toString());
+                        post.setBody(editTextContent.getText().toString());
+                        post.setUserId(Integer.parseInt(editTextUserId.getText().toString()));
+                        postService.update(id, post).enqueue(new Callback<Post>() {
+                            @Override
+                            public void onResponse(Call<Post> call, Response<Post> response) {
+                                if (response.isSuccessful()) {
+                                    Log.i("SinglePost", " Code: " + response.code());
+                                    alertDialog.cancel();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<Post> call, Throwable t) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+        findViewById(R.id.deletePost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postService.deletePost(id).enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(Call<Post> call, Response<Post> response) {
+                        Log.i("SinglePost", "Code : " + response.code());
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Post> call, Throwable t) {
+
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/android/dmaharjan/androidrest/service/PostService.java
+++ app/src/main/java/com/android/dmaharjan/androidrest/service/PostService.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 /**
@@ -29,4 +31,10 @@ public interface PostService {
 
     @POST("/posts")
     Call<Post> saveNewPost(@Body Post post);
+
+    @PUT("/posts/{id}")
+    Call<Post> update(@Path("id") Integer id, @Body Post post);
+
+    @DELETE("/posts/{id}")
+    Call<Post> deletePost(@Path("id") Integer id);
 }
